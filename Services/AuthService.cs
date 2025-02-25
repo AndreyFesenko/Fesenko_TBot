eef6d7b032cbe8b2b8e21d64b659beb28e606803
AndreyFesenko <fes256@gmail.com>
2025-02-25 19:26:20 +0300

    Exam

--- Services/AuthService.cs
+++ Services/AuthService.cs
@@ -14,15 +14,6 @@ namespace Fesenko_TBot.Services
         }
 
         // Метод для хэширования пароля
-        //private string HashPassword(string password)
-        //{
-        //    using (var sha256 = SHA256.Create())
-        //    {
-        //        var hashedBytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(password));
-        //        return BitConverter.ToString(hashedBytes).Replace("-", "").ToLower();
-        //    }
-        //}
-
         private string HashPassword(string password)
         {
             byte[] salt = new byte[16];
